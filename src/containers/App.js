957ab4309df4ec63cd511ef7d8e555d17e91d9ec
Damian Godek <damian.godek1@gmail.com>
2018-11-21 18:21:04 +0100

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
     }
     
     removeTodo(id) {
-        const remainder = this.state.data.filter(todo => todo.id !== todo.id);
+        const remainder = this.state.data.filter(todo => todo.id !== id);
         this.setState({data: remainder})
     }
     
